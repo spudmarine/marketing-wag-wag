c29bcf768d2eb5f23448fe8dbd42e4155fa430f8
Jenny <jenmjayne@gmail.com>
2014-12-06 15:11:52 -0600

    first controllers and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 MarketingWagWag::Application.routes.draw do
+
+  root to: 'static_page#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
