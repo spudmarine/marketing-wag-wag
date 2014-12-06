d5eea93a6132bece30ef15fa061965f823cc5aed
Jenny <jenmjayne@gmail.com>
2014-12-06 14:54:26 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketingWagWag::Application.config.secret_key_base = 'ffefa0870ee745667c318a9f8bd28559b649197bc2d0ecac9910008ecb99b7b6bac8c2dac20933ca84f0109bf892e1c48b938f1d2350e55fdd1e0ce42c3bf60f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarketingWagWag::Application.config.session_store :cookie_store, key: '_marketing-wag-wag_session'
