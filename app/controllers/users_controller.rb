7aef116e62821cf8782688b882ff1f8477c3e25e
Scott Rowland <scrowlan@gmail.com>
2014-12-14 19:01:21 -0600

    update to user controller for error on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
         format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
+        format.html { redirect_to root_path, notice: 'There was a problem.' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
